c3c9ec69cecca643847359232d99bcb1f8919d2a
whitecherry <jsh010322@gmail.com>
2021-12-22 13:35:39 +0900

    import

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Board {
 
     TetrisPiece currentPiece;
--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 
 class MainClass {
     public static void main(String[] args) {
